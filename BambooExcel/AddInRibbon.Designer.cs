a9636d90aea0217e0638e5175958233ca5a44a71
张杰 <165590438@qq.com>
2017-12-19 14:32:22 +0800

    1

---
+++ BambooExcel/AddInRibbon.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace BambooExcel
+{
+    partial class AddInRibbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public AddInRibbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.grpFileSystem = this.Factory.CreateRibbonGroup();
+            this.btnReplaceTextInFiles = this.Factory.CreateRibbonButton();
+            this.grpView = this.Factory.CreateRibbonGroup();
+            this.btnDocExplorerPane = this.Factory.CreateRibbonToggleButton();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.editBox1 = this.Factory.CreateRibbonEditBox();
+            this.tab1.SuspendLayout();
+            this.grpFileSystem.SuspendLayout();
+            this.grpView.SuspendLayout();
+            this.group1.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.grpFileSystem);
+            this.tab1.Groups.Add(this.grpView);
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Label = "悉道工具栏";
+            this.tab1.Name = "tab1";
+            // 
+            // grpFileSystem
+            // 
+            this.grpFileSystem.Items.Add(this.btnReplaceTextInFiles);
+            this.grpFileSystem.Label = "File System";
+            this.grpFileSystem.Name = "grpFileSystem";
+            // 
+            // btnReplaceTextInFiles
+            // 
+            this.btnReplaceTextInFiles.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btnReplaceTextInFiles.Description = "系统初始化";
+            this.btnReplaceTextInFiles.Label = "系统初始化";
+            this.btnReplaceTextInFiles.Name = "btnReplaceTextInFiles";
+            this.btnReplaceTextInFiles.OfficeImageId = "ReplaceDialog";
+            this.btnReplaceTextInFiles.ShowImage = true;
+            this.btnReplaceTextInFiles.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnReplaceTextInFiles_Click);
+            // 
+            // grpView
+            // 
+            this.grpView.Items.Add(this.btnDocExplorerPane);
+            this.grpView.Label = "View";
+            this.grpView.Name = "grpView";
+            // 
+            // btnDocExplorerPane
+            // 
+            this.btnDocExplorerPane.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btnDocExplorerPane.Label = "前序高亮";
+            this.btnDocExplorerPane.Name = "btnDocExplorerPane";
+            this.btnDocExplorerPane.ShowImage = true;
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.editBox1);
+            this.group1.Label = "group1";
+            this.group1.Name = "group1";
+            // 
+            // editBox1
+            // 
+            this.editBox1.Label = "editBox1";
+            this.editBox1.Name = "editBox1";
+            this.editBox1.Text = null;
+            // 
+            // AddInRibbon
+            // 
+            this.Name = "AddInRibbon";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.AddInRibbon_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.grpFileSystem.ResumeLayout(false);
+            this.grpFileSystem.PerformLayout();
+            this.grpView.ResumeLayout(false);
+            this.grpView.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup grpFileSystem;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup grpView;
+        internal Microsoft.Office.Tools.Ribbon.RibbonToggleButton btnDocExplorerPane;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnReplaceTextInFiles;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox editBox1;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal AddInRibbon AddInRibbon
+        {
+            get { return this.GetRibbon<AddInRibbon>(); }
+        }
+    }
+}
---
+++ BambooExcel/AddInRibbon.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using System.Windows.Forms;
+using Microsoft.Office.Tools;
+using BambooExcel.Forms;
+using Microsoft.Office.Interop.Excel;
+using BambooExcel.Helpers;
+
+
+
+namespace BambooExcel
+{
+    public partial class AddInRibbon
+    {
+
+        //TODO:  Need to unload all COM
+
+        private void AddInRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+           
+        }
+
+        void ws_SelectionChange(Range Target)
+        {
+            try
+            {
+                if (Target.Text == "")
+                    return;
+                MessageBox.Show(Target.Text);
+                Microsoft.Office.Interop.Excel.Application excelApp = Globals.ThisAddIn.Application;
+                Workbook wb = ExcelHelper.GetActiveWorkbook(true, excelApp);
+                Worksheet ws = null;
+                if (wb == null)
+                    return;
+                Object temp;
+
+                foreach (Worksheet wstemp in wb.Worksheets)
+                {
+                    ws = wstemp;
+                    break;
+                }
+                if (ws == null)
+                    return;
+                int rowcount = ws.UsedRange.Rows.Count;
+                int colcount = ws.UsedRange.Columns.Count;
+                for (int i = 1; i <= rowcount; i++)
+                {
+                    for (int j = 1; j <= colcount; j++)
+                    {
+                        ((Range)ws.Cells[i, j]).Font.ColorIndex = 1; 
+                    }
+                }
+                for (int i = 1; i <= rowcount; i++)
+                {
+                    for (int j = 1; j <= colcount; j++)
+                    {
+                        if (ws.Cells[i, j].Text == Target.Text)
+                        {
+                            ((Range)ws.Cells[i, j]).Font.ColorIndex = 3; 
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private void btnImportFileList_Click(object sender, RibbonControlEventArgs e)
+        {
+            //frmFileListOptions frm = new frmFileListOptions();
+            //frm.Show();
+
+        }
+
+
+        void ws_BeforeDoubleClick(Range Target, ref bool Cancel)
+        {
+            MessageBox.Show("d");
+        }
+
+        private void btnReplaceTextInFiles_Click(object sender, RibbonControlEventArgs e)
+        {
+            Microsoft.Office.Interop.Excel.Application excelApp = Globals.ThisAddIn.Application;
+            Workbook wb = ExcelHelper.GetActiveWorkbook(true, excelApp);
+            foreach(Worksheet ws in wb.Worksheets)
+            {
+                ws.BeforeDoubleClick += ws_BeforeDoubleClick;
+                ws.SelectionChange+=ws_SelectionChange;
+            }
+            MessageBox.Show("初始化成功");
+        }
+
+        private void btnUnprotectWB_Click(object sender, RibbonControlEventArgs e)
+        {
+            MessageBox.Show("12");
+            Helpers.SecurityHelper.UnprotectWorkbook();
+        }
+
+        private void btnImportDirList_Click(object sender, RibbonControlEventArgs e)
+        {
+            
+        }
+
+
+    }
+}
