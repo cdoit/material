65cf23eef8087286713aa5d1d878deaef9dc6fb1
ZhangxfKKKP <zhang.xiangfei@cdoplus.com>
2018-01-03 10:28:02 +0800

    123

--- BambooExcel/AddInRibbon.cs
+++ BambooExcel/AddInRibbon.cs
@@ -8,6 +8,8 @@ using Microsoft.Office.Tools;
 using BambooExcel.Forms;
 using Microsoft.Office.Interop.Excel;
 using BambooExcel.Helpers;
+using MySql.Data.MySqlClient;
+using BambooExcel.Mod;
 
 
 
@@ -148,10 +150,138 @@ namespace BambooExcel
 
         
 
+        //导入设计包
         private void toggleButton1_Click(object sender, RibbonControlEventArgs e)
         {
+            try
+            {
+                Microsoft.Office.Interop.Excel.Application excelApp = Globals.ThisAddIn.Application;
+                Workbook wb = ExcelHelper.GetActiveWorkbook(true, excelApp);
+                Worksheet ws = null;
+                if (wb == null)
+                    return;
+                Object temp;
+                //获取第一个工作簿
+                ws = wb.Worksheets[2];
+
+
+                //foreach (Worksheet wstemp in wb.Worksheets)
+                //{
+                //    ws = wstemp;
+                //    break;
+                //}
+
+
+                if (ws == null)
+                    return;
+
+                //定义集合(工作簿1数据)
+                Dictionary<int, List<String>> dic = new Dictionary<int, List<String>>();
+                int rowcount = ws.UsedRange.CurrentRegion.Rows.Count;
+                MessageBox.Show("列：" + rowcount);
+                int colcount = ws.UsedRange.CurrentRegion.Rows.Count;
+                MessageBox.Show("行：" + colcount);
+
+                for (int i = 3; i <= colcount; i++)
+                {
+                    List<String> strList = new List<String>();
+                    houseplan houseplan = new houseplan();
+                    houseplan.Id = i + 14;
+                    for (int j = 2; j <= rowcount; j++)
+                    {
+                        if (!string.IsNullOrEmpty(ws.Cells[i, j].Text) && j != 4)
+                        {
+                            houseplan.Planname = "壹号院方案";
+                            //strList.Add(ws.Cells[i, j].Text);
+                        }
+                        if (!string.IsNullOrEmpty(ws.Cells[i, j].Text) && j == 2)
+                        {
+                            houseplan.Room = ws.Cells[i, j].Text;
+                        }
+                        if (!string.IsNullOrEmpty(ws.Cells[i, j].Text) && j == 3)
+                        {
+                            houseplan.Part = ws.Cells[i, j].Text;
+                        }
+                        if (!string.IsNullOrEmpty(ws.Cells[i, j].Text) && j == 5)
+                        {
+                            houseplan.Materialpackge1 = ws.Cells[i, j].Text;
+                        }
+
+                    }
+                    //插数据
+                    insertinto(houseplan);
+                    dic.Add(i, strList);
+                }
+
+                //MessageBox.Show(list.Count + "");
+
+
+                //工作簿3的数据
+                //Worksheet ws3 = wb.Worksheets[2];
+                //int ws3count = ws3.UsedRange.CurrentRegion.Rows.Count;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+
+
+
+        //建立mysql数据库链接
+        public static MySqlConnection getMySqlCon()
+        {
+            MySqlConnection mysql = null;
+            try
+            {
+                String mysqlStr = "Database=cdomaterial;Data Source=127.0.0.1;User Id=root;Password=root;pooling=false;CharSet=utf8;port=3306";
+                // String mySqlCon = ConfigurationManager.ConnectionStrings["MySqlCon"].ConnectionString;
+                mysql = new MySqlConnection(mysqlStr);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return mysql;
+        }
+
+        //建立执行命令语句对象
+        public static MySqlCommand getSqlCommand(String sql, MySqlConnection mysql)
+        {
+            MySqlCommand mySqlCommand = null;
+            try
+            {
+                mySqlCommand = new MySqlCommand(sql, mysql);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            //  MySqlCommand mySqlCommand = new MySqlCommand(sql);
+            // mySqlCommand.Connection = mysql;
+            return mySqlCommand;
+        }
+
+
+        //添加数据
+        public static void getInsert(MySqlCommand mySqlCommand)
+        {
+            try
+            {
+                mySqlCommand.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                String message = ex.Message;
+                Console.WriteLine("插入数据失败了！" + message);
+                MessageBox.Show("插入数据失败了！" + message);
+            }
 
         }
+
 
 
     }
---
+++ BambooExcel/Mod/houseplan.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BambooExcel.Mod
+{
+    public partial class houseplan
+    {
+        public int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public String planname;
+
+        public String Planname
+        {
+            get { return planname; }
+            set { planname = value; }
+        }
+
+        public String room;
+
+        public String Room
+        {
+            get { return room; }
+            set { room = value; }
+        }
+        public String part;
+
+        public String Part
+        {
+            get { return part; }
+            set { part = value; }
+        }
+        public String materialpackge;
+
+        public String Materialpackge1
+        {
+            get { return materialpackge; }
+            set { materialpackge = value; }
+        }
+
+
+
+
+
+    }
+}
