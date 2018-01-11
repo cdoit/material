c8fc8c7d3a1e4ff87a0bb0f05aab3d32a6343bd4
张杰 <165590438@qq.com>
2018-01-11 11:45:31 +0800

    12

--- BambooExcel/matrail/BaseMatrail.cs
+++ BambooExcel/matrail/BaseMatrail.cs
@@ -13,10 +13,10 @@ namespace BambooExcel.matrail
         public bool import(string file)
         {
             bool bok = false;
-           
+
             XmlDocument doc = new System.Xml.XmlDocument();
             doc.Load(file);
-            XmlNode root=findroot(doc.FirstChild);
+            XmlNode root = findroot(doc.FirstChild);
             if (MessageBox.Show("清空物料基础数据，是否继续？", "提示", MessageBoxButtons.YesNo) == DialogResult.No)
                 return false;
             MySqlCommand cmd = Application.instance().myConnection.CreateCommand();
@@ -30,24 +30,24 @@ namespace BambooExcel.matrail
             {
                 string id = child.Attributes["ID"].Value;
                 string nodetext = child.Attributes["TEXT"].Value;
-                string[] arry=nodetext.Split('【');
+                string[] arry = nodetext.Split('【');
                 if (arry.Length == 2)
                 {
                     string name = arry[0];
                     string num = arry[1];
-                    num = num.Replace("】","");
-                    loadlevel("-1","", child);
+                    num = num.Replace("】", "");
+                    loadlevel("-1", "", child);
                 }
-                
+
             }
-             
+
             return bok;
         }
 
         private XmlNode findroot(XmlNode node)
         {
-            XmlNode textnode=node.Attributes.GetNamedItem("TEXT");
-            if (textnode !=null&& node.Attributes["TEXT"].Value == "中心主题")
+            XmlNode textnode = node.Attributes.GetNamedItem("TEXT");
+            if (textnode != null && node.Attributes["TEXT"].Value == "中心主题")
             {
                 return node;
             }
@@ -94,13 +94,13 @@ namespace BambooExcel.matrail
                 }
             }
             return result;
-        } 
-        private void loadlevel(string parentid,string parentnum,XmlNode node)
+        }
+        private void loadlevel(string parentid, string parentnum, XmlNode node)
         {
             //string id = node.Attributes["ID"].Value;
-            string nodetext= node.Attributes["TEXT"].Value;
-            nodetext=nodetext.Replace(" ", "");
-            string[] arry=nodetext.Split('【');
+            string nodetext = node.Attributes["TEXT"].Value;
+            nodetext = nodetext.Replace(" ", "");
+            string[] arry = nodetext.Split('【');
             if (arry.Length == 2)
             {
                 string name = arry[0];
@@ -142,15 +142,38 @@ namespace BambooExcel.matrail
 
                             }
                         }
-     
+                        string[] bianlianarry = bianli(res);
+                        foreach (string onecell in bianlianarry)
+                        {
+                            string selfid = parentnum;
+                            string[] cellarry = onecell.Split('/');
+                            string code = "";
+                            string attribute = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?><row>";
+                            for (int i = 0; i < cellarry.Length - 2; i++)
+                            {
+                                attribute += "<field name=" + cellarry[i].Trim() + ">" + cellarry[i + 1].Trim() + "</field>";
+                                selfid += cellarry[i + 2].Trim();
+                                i = i + 2;
+                            }
+                            attribute += "</row>";
+                            string codetemp = selfid.Substring(selfid.IndexOf(parentnum) + parentnum.Length);
+                            selfid = selfid.PadRight(15, '0');
+                            for (int j = 0; j < codetemp.Length; j++)
+                            {
+                                code += codetemp[j] + "_";
+                            }
+                            cmd = Application.instance().myConnection.CreateCommand();
+                            cmd.CommandText = "INSERT  INTO material(id,categoryid,name,specifications,attributeinfo)VALUES ('" + selfid + "','" + parentnum + "','" + "" + "','" + code + "','" + attribute + "')";
+                            cmd.ExecuteNonQuery();
+                        }
                         return;
                     }
                 }
             }
-           
-            if(node.HasChildNodes == false)
+
+            if (node.HasChildNodes == false)
                 return;
-            foreach(XmlNode child in node.ChildNodes)
+            foreach (XmlNode child in node.ChildNodes)
             {
 
                 loadlevel(parentnum, parentnum, child);
