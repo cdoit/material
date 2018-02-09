803a55c1f2c72657c7c73728bdb4fbcc8f0c8f44
ZhangxfKKKP <zhang.xiangfei@cdoplus.com>
2018-02-09 15:53:56 +0800

    2222

---
+++ BambooExcel/Mod/component.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BambooExcel.Mod
+{
+    public partial class component
+    {
+        private String id;
+
+        public String Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private String projectid;
+
+        public String Projectid
+        {
+            get { return projectid; }
+            set { projectid = value; }
+        }
+
+        private String name;
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+}
---
+++ BambooExcel/Mod/meterage.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BambooExcel.Mod
+{
+    public partial class meterage
+    {
+        private String id;
+
+        public String Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private String parentid;
+
+        public String Parentid
+        {
+            get { return parentid; }
+            set { parentid = value; }
+        }
+
+        private String componentid;
+
+        public String Componentid
+        {
+            get { return componentid; }
+            set { componentid = value; }
+        }
+
+        private String materielcode;
+
+        public String Materielcode
+        {
+            get { return materielcode; }
+            set { materielcode = value; }
+        }
+
+        private String materielname;
+
+        public String Materielname
+        {
+            get { return materielname; }
+            set { materielname = value; }
+        }
+
+        private String specifications;
+
+        public String Specifications
+        {
+            get { return specifications; }
+            set { specifications = value; }
+        }
+
+        private String count;
+
+        public String Count
+        {
+            get { return count; }
+            set { count = value; }
+        }
+
+        private String unit;
+
+        public String Unit
+        {
+            get { return unit; }
+            set { unit = value; }
+        }
+
+        private String rule;
+
+        public String Rule
+        {
+            get { return rule; }
+            set { rule = value; }
+        }
+
+        private String lossrate;
+
+        public String Lossrate
+        {
+            get { return lossrate; }
+            set { lossrate = value; }
+        }
+
+
+        private String memo;
+
+        public String Memo
+        {
+            get { return memo; }
+            set { memo = value; }
+        }
+
+    }
+}
