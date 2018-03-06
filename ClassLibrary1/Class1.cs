c00e79da9f2136a4c12e06a7d7b52e356e3ea88e
张杰 <165590438@qq.com>
2018-03-06 11:44:00 +0800

    121

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+namespace IronPython_TestDll
+{
+    public class TestDll
+    {
+        public  string Add(string json)
+        {        
+            CDO.BaiJuyi.Tym.TymCommand command = new CDO.BaiJuyi.Tym.TymCommand();
+            var ms = command.Excute(json);
+            return ms;
+        }
+    }
+
+
+    public class TestDll1
+    {
+        private int aaa = 11;
+        public int AAA
+        {
+            get { return aaa; }
+            set { aaa = value; }
+        }
+        public void ShowAAA()
+        {
+            
+        }
+
+
+    }
+}
